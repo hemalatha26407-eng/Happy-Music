bd6e5dc138b731e768e018e01eef3e232d9d38a1
hemalatha26407-eng <hemalatha26407@gmail.com>
2026-06-12 12:11:19 +0530

    Add files via upload

---
+++ Browse.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import SongCard from "../components/SongCard";
+
+import songs from "../data/songs";
+
+const Browse = () => {
+
+  const [search, setSearch] = useState("");
+
+  const allSongs = [
+
+    ...songs.happy,
+    ...songs.sad,
+    ...songs.chill,
+    ...songs.love,
+    ...songs.focus
+
+  ];
+
+  const filteredSongs = allSongs.filter((song) =>
+    song.title
+      .toLowerCase()
+      .includes(search.toLowerCase())
+  );
+
+  return (
+
+    <div>
+
+      <h1 className="page-title">
+        🎧 Browse Songs
+      </h1>
+
+      <input
+        type="text"
+        placeholder="Search songs..."
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+        className="search-box"
+      />
+
+      <div className="songs-container">
+
+        {filteredSongs.map((song, index) => (
+
+          <SongCard
+            key={index}
+            song={song}
+          />
+
+        ))}
+
+      </div>
+
+    </div>
+
+  );
+};
+
+export default Browse;
---
+++ Sidebar.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+
+    <div className="sidebar">
+
+      <h2 className="logo">
+        🎵 Beats
+      </h2>
+
+      <ul>
+
+        <li>
+          <Link to="/">🏠 Home</Link>
+        </li>
+
+        <li>
+          <Link to="/trending">🔥 Trending</Link>
+        </li>
+
+        <li>
+          <Link to="/favorites">❤️ Favorites</Link>
+        </li>
+
+        <li>
+          <Link to="/browse">🎧 Browse</Link>
+        </li>
+
+        <li>
+          <Link to="/playlists">📂 Playlists</Link>
+        </li>
+
+        <li>
+          <Link to="/profile">👤 Profile</Link>
+        </li>
+
+        <li>
+  <Link to="/recent">
+    🕒 Recent
+  </Link>
+</li>
+
+<li>
+  <Link to="/create">
+    ➕ Create Playlist
+  </Link>
+</li>
+
+      </ul>
+
+    </div>
+
+  );
+};
+
+export default Sidebar;
